1c33dcab5d818efcc1a97baf2044a4eee743886e
namgigun <84756980+namgigun@users.noreply.github.com>
2025-02-21 18:24:28 +0900

    create : 프로젝트 생성
    1. 프로젝트 세팅 및 작동확인

---
+++ src/main/java/myProject/searchMusical/SearchMusicalApplication.java
@@ -0,0 +1,12 @@
+package myProject.searchMusical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class SearchMusicalApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SearchMusicalApplication.class, args);
+	}
+}
