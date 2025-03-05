e570e49c32a0114f2f1d25d7cfed808704430ec8
namgigun <84756980+namgigun@users.noreply.github.com>
2025-03-05 22:21:36 +0900

    refactor : log4j2 삭제

--- src/main/java/myProject/searchMusical/SearchMusicalApplication.java
+++ src/main/java/myProject/searchMusical/SearchMusicalApplication.java
@@ -1,14 +1,13 @@
 package myProject.searchMusical;
 
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@Log4j2
+@Slf4j
 public class SearchMusicalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SearchMusicalApplication.class, args);
--- src/test/java/myProject/searchMusical/repository/WebDriverTest.java
+++ src/test/java/myProject/searchMusical/repository/WebDriverTest.java
@@ -1,20 +1,14 @@
 package myProject.searchMusical.repository;
 
-import lombok.extern.log4j.Log4j2;
-import myProject.searchMusical.entity.Post;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.Duration;
-import java.util.List;
-
 @SpringBootTest
-@Log4j2
+@Slf4j
 @Transactional
 public class WebDriverTest {
 
